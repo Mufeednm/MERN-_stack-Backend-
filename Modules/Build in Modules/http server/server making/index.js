4ad22c8bb2ed09bc138bc6ace7ce8747c56c5730
mufeednm <mufeedmusthafanm@gmail.com>
2024-05-08 19:46:36 +0530

    json server

--- Modules/Build in Modules/http server/server making/index.js
+++ Modules/Build in Modules/http server/server making/index.js
@@ -7,22 +7,22 @@
 // server.listen(3001,()=>{
 //     console.log("SERVER RUNNING ON TIMEj");
 // })
-                //   json server
-                // const http = require ("node:http");
-                // const { json } = require("stream/consumers");
+//                   json server
+                const http = require ("node:http");
+                const { json } = require("stream/consumers");
                 
-                // const server = http.createServer((req,res)=>{
-                //     const superhero ={
-                //         firstname:"mufeed",
-                //         lastname : "babu"
-                //     };
+                const server = http.createServer((req,res)=>{
+                    const superhero ={
+                        firstname:"mufeed",
+                        lastname : "babu"
+                    };
                 
-                //     res.writeHead(200, {"content-type": "application/json" })
-                //     res.end(JSON.stringify(superhero))
-                // }) 
-                // server.listen(3000,()=>{
-                //     console.log("SERVER RUNNING ON TIMEj");
-                // })
+                    res.writeHead(200, {"content-type": "application/json" })
+                    res.end(JSON.stringify(superhero))
+                }) 
+                server.listen(3000,()=>{
+                    console.log("SERVER RUNNING ON TIMEj");
+                })
     //   html response 
 //     const http = require ("node:http");
 //     const fs = require ("node:fs")
@@ -43,19 +43,19 @@
 //     console.log("SERVER RUNNING ON TIMEj");
 // })
         //   html+ js  changes
-        const http = require ("node:http");
-    const fs = require ("node:fs")
-const { json } = require("stream/consumers");
+//         const http = require ("node:http");
+//     const fs = require ("node:fs")
+// const { json } = require("stream/consumers");
 
-const server = http.createServer((req,res)=>{
-    const superhero ="mufeed"
+// const server = http.createServer((req,res)=>{
+//     const superhero ="mufeed"
 
-    res.writeHead(200, {"content-type": "text/html" })
-    // fs.createReadStream("./index.html").pipe(res)
-    let html = fs.readFileSync("./index.html", "utf-8")
-    html=html.replace("{{name}}",superhero)
-    res.end(html);
-}) 
-server.listen(3000,()=>{
-    console.log("SERVER RUNNING ON TIMEj");
-})
+//     res.writeHead(200, {"content-type": "text/html" })
+//     // fs.createReadStream("./index.html").pipe(res)
+//     let html = fs.readFileSync("./index.html", "utf-8")
+//     html=html.replace("{{name}}",superhero)
+//     res.end(html);
+// }) 
+// server.listen(3000,()=>{
+//     console.log("SERVER RUNNING ON TIMEj");
+// })
