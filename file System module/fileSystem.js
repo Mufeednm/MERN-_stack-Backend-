2c2579aa8ade437c01fc486d545ff1e6853497d6
mufeednm <mufeedmusthafanm@gmail.com>
2024-05-11 10:17:41 +0530

    file system

---
+++ file System module/fileSystem.js
@@ -0,0 +1,27 @@
+// // read File
+var fs = require("fs");
+
+// fs.readFile("./text.txt","utf-8",function(err,data){
+//     // the f of read file should be caps
+//     if (err) {
+//         console.log("its error");
+        
+//     }
+//     console.log(data);
+// });
+
+
+//  create file 
+// fs.writeFile("new.txt","it is done by create file by mufeed musthafa", (err)=>{
+//     if (err) {
+//         console.log("its an eroor");
+        
+//     }
+//     console.log("file created");
+// })         Another way (appendFiles)
+fs.appendFile ("new.txt","it is done by create file by mufeed musthafa by append  \n", (err)=>{
+    if (err) {
+        console.log("its an eroor");
+        
+    }
+    console.log("file created");})  
