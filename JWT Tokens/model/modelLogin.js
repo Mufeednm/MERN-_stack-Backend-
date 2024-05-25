e1b2bb87ad2b2a03ad93d1aff360ee21e01c309f
mufeednm <mufeedmusthafanm@gmail.com>
2024-05-25 08:48:38 +0530

    jwt token is ready but not added in get

--- JWT Tokens/model/modelLogin.js
+++ JWT Tokens/model/modelLogin.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose")
 const LoginSchema = mongoose.Schema({
     name:String,
+    email : String,
     password:String
 })
 module.exports= mongoose.model("SignIN",LoginSchema )
--- JWT Tokens/server.js
+++ JWT Tokens/server.js
@@ -7,37 +7,52 @@ const  SIGNIN = require("./model/modelLogin")
 const { create } = require("moongose/models/user_model")
 const database =mongoose.connect("mongodb://localhost:27017/LOGINDATAS")
 const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 app.use(express.json())
-app.get("/", async (req,res)=>{
-const serverData = await SIGNIN.find()
- res.send( "hello ")
- console.log(serverData);
-})
+
+
 app.post("/signIn",async(req,res)=>{
-    const {name,password} = req.body
+    const {name,email,password} = req.body
     
       const  salt = await bcrypt.genSalt()
       const hashpassword = await bcrypt.hash(password,salt)  
     
 let Datasend= await SIGNIN.create({
  name : name ,
+ email:email,
 password:hashpassword
 }) 
 
     res.status(201).json({message:"data send"})
     // console.log(userData);
 })
+
+
+
 app.post("/logIn", async (req,res)=>{
   
-    const { name, password } = req.body;
+    const {email,password } = req.body;
 
-    const logindata=  await SIGNIN.findOne(name)
-    if (!logindata) {
+const emailVarify=  await SIGNIN.findOne({email})
+    if (!emailVarify) {
         return res.status(400).json({ message: "User not found" });
       
     }
-    console.log("user found");
+const passwordVarify = bcrypt.compareSync(password,emailVarify.password)
+if(!passwordVarify) return res.status(401).json({ staus: "error",  message: "Wrong credentials" });
+    // console.log(passwordVarify);
     
+    //jwt
+    const token = jwt.sign({id : emailVarify._id},process.env.USER_JWT_SECRET,{expiresIn:"1h"})
+          // cookie setting 
+          res.cookie('access_token', token, {
+             httpOnly: true})
+          .status(200).json({ status: "Ok", message:"User login succfully", token })
+
+})
+
+app.get("/",(req,res)=>{
+res.send("hello")
 })
 
 
